af35f569f5787de3860782d9cbc607487f9ebf50
Maelstromage <Reverse91@yahoo.com>
2019-05-28 10:30:37 -0400

    Update get.ps1

--- xfer/scripts/get.ps1
+++ xfer/scripts/get.ps1
@@ -23,7 +23,9 @@ function Get-ValidEntry {
 
 if(!(Test-Path $confLocation -PathType Leaf)){ #returns true or false if false proceeds
     Write-Host "Cannot find compxfer.conf in location $scriptRoot. Please move compxfer.conf to $scriptRoot" -fore Red
+    if ($gRemote -eq "TRUE"){exit}else{
     read-host "press any key to continue"
+    }
     exit
 }
 
@@ -193,19 +195,22 @@ foreach ($configLine in $compXfer) {
 
 ### Overwrite Comps file
 
-if(!(Test-Path "$cPath" -PathType Leaf)){ #returns true or false if false proceeds
+if ($gRemote -eq "TRUE"){
+Clear-Content -Path $cPath -Force
+continue}else{
+    if(!(Test-Path "$cPath" -PathType Leaf)){ #returns true or false if false proceeds
     
-} else {
-$errorRead = Read-Host "File $cPath exists, would you like to Overwrite? (y/n)"
-    if($errorRead -eq "y"){
-        Clear-Content -Path $cPath -Force
     } else {
-        Write-Host "File not overwritten, exiting..."
-        read-host "press any key to continue"
-        Exit
+    $errorRead = Read-Host "File $cPath exists, would you like to Overwrite? (y/n)"
+        if($errorRead -eq "y"){
+            Clear-Content -Path $cPath -Force
+        } else {
+            Write-Host "File not overwritten, exiting..."
+            read-host "press any key to continue"
+            Exit
+        }
     }
 }
-
 ### Gets Hostname and Username
 
 Add-Content $cPath "HostName = $comp"
@@ -314,83 +319,5 @@ if($chromePasswords -eq "TRUE"){
 
 
 
-read-host "press any key to continue"
+if ($gRemote -eq "TRUE"){exit}else{read-host "press enter key to continue"}
 
-
-<#
-### Backup of install Script.
-
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-Write-Output $PSScriptRoot
-New-Item -Path $PSScriptRoot -name "comps" -ItemType "directory"
-New-Item -Path $PSScriptRoot -name "export" -ItemType "directory"
-New-Item -Path $PSScriptRoot -name "installs" -ItemType "directory"
-Write-Output "Downloading psexec..."
-$url = "https://download.sysinternals.com/files/PSTools.zip"
-$output = "$PSScriptRoot\PSTools.zip"
-Write-Output "Download Complete."
-Invoke-WebRequest -uri $url -OutFile $output
-Write-Output "Unziping PSTools.zip..."
-Unzip "$PSScriptRoot\PSTools.zip" "$PSScriptRoot\PSTools"
-Write-Output "Unzip Complete."
-Remove-Item "$PSScriptRoot\pstools.zip"
-Move-Item -Path $PSScriptRoot\xfer\docs -Destination $PSScriptRoot
-
-
-New-Item -Path $PSScriptRoot -name "scripts" -ItemType "directory"
-Move-Item -Path $PSScriptRoot\xfer\scripts\put1.ps1 -Destination "$PSScriptRoot\scripts"
-Move-Item -Path $PSScriptRoot\xfer\scripts\put2.ps1 -Destination "$PSScriptRoot\scripts"
-
-
-$getLocal = 'PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '+"'"+ '-NoProfile -ExecutionPolicy Unrestricted -File ""' + "$PSScriptRoot\scripts\get.ps1" + '""'+"'"+'}";' + "`r`n" + 'pause'
-New-Item -Path $PSScriptRoot -name "getlocal.bat" -ItemType "file" -Value $getLocal
-
-$getRemote = 'set /p comp=Please enter the computer name and hit enter:' + "`r`n" + 'echo Executing on %comp%...'+ "`r`n" + 'psexec -accepteula -i -s \\%comp% cscript "' + "$PSScriptRoot\scripts\remoteget.vbs" + '"' + "`r`n" + 'pause'
-New-Item -path $PSScriptRoot -name "getremote.bat" -ItemType "file" -Value $getRemote
-
-$getPut = 'PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '+ "'" + '-NoProfile -ExecutionPolicy Unrestricted -File ""' + "$PSScriptRoot\scripts\put1.ps1" + '""' +"'" + '}";' + "`r`n" + 'pause'
-New-Item -path $PSScriptRoot -name "put.bat" -ItemType "file" -Value $getPut
-
-
-
-Move-Item -Path $PSScriptRoot\xfer\scripts\get.ps1 -Destination "$PSScriptRoot\scripts"
-
-
-((Get-Content -path "$PSScriptRoot\scripts\get.ps1" -Raw) -replace 'REPLACEME',$PSScriptRoot) | Set-Content -Path "$PSScriptRoot\scripts\get.ps1"
-Get-Content -path "$PSScriptRoot\scripts\get.ps1"
-
-
-
-
-$remoteGet = 'start /min PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList ' + "'" + '-windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted -File ""' + "$PSScriptRoot\scripts\remoteget.ps1" + '""' +"'" + '}";'
-
-New-Item -path "$PSScriptRoot\scripts" -name "remoteget.bat" -ItemType "file" -Value $remoteGet
-
-
-
-((Get-Content -path "$PSScriptRoot\xfer\scripts\remoteget.vbs" -Raw) -replace 'REPLACEME',$PSScriptRoot) | Set-Content -Path "$PSScriptRoot\scripts\remoteget.vbs"
-Get-Content -path "$PSScriptRoot\scripts\remoteget.vbs"
-
-
-
-((Get-Content -path "$PSScriptRoot\xfer\scripts\remoteget.ps1" -Raw) -replace 'REPLACEME',$PSScriptRoot) | Set-Content -Path "$PSScriptRoot\scripts\remoteget.ps1"
-Get-Content -path "$PSScriptRoot\scripts\remoteget.ps1"
-
-
-
-((Get-Content -path "$PSScriptRoot\xfer\scripts\compxfer.conf" -Raw) -replace 'REPLACEME',$PSScriptRoot) | Set-Content -Path "$PSScriptRoot\scripts\compxfer.conf"
-Get-Content -path "$PSScriptRoot\scripts\compxfer.conf"
-Remove-Item -Path "$PSScriptRoot\xfer" -Recurse -force
-
-$argList = "-NoProfile -ExecutionPolicy Unrestricted -command remove-item " + '"' + $PSScriptRoot + '\install.ps1' + '"'
-
-Start-Process PowerShell -ArgumentList $argList
-#>
