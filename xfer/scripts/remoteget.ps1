64eaf041986de91ca562e0647fbaf495513b3290
Maelstromage <Reverse91@yahoo.com>
2019-05-28 10:21:36 -0400

    Update remoteget.ps1

--- xfer/scripts/remoteget.ps1
+++ xfer/scripts/remoteget.ps1
@@ -1,3 +1,2 @@
 # Written by Harley Schaeffer version 1.2
-# Write-host "Version 1.2 written by Harley Schaeffer. Please feel free to email Harley.Schaeffer@assaabloy.com with any issues." -fore Gray
 cmd /c start /min PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted -File ""\\usblns-file2\get\dev\scripts\get.ps1"" -gRemote TRUE'}";
