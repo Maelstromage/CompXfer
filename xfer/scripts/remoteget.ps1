834db8750e08c55e831f50565fc5c5434ac23f15
Maelstromage <Reverse91@yahoo.com>
2019-05-28 10:33:39 -0400

    Update remoteget.ps1

--- xfer/scripts/remoteget.ps1
+++ xfer/scripts/remoteget.ps1
@@ -1,2 +1,2 @@
 # Written by Harley Schaeffer version 1.2
-cmd /c start /min PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted -File ""\\usblns-file2\get\dev\scripts\get.ps1"" -gRemote TRUE'}";
+cmd /c start /min PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted -File ""REPLACEME\scripts\get.ps1"" -gRemote TRUE'}";
