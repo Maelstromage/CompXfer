a3d6e4155bbd1dd846fbdb693ea23cde621e9b69
Maelstromage <Reverse91@yahoo.com>
2019-05-29 09:43:37 -0400

    Update remoteget.ps1

--- xfer/scripts/remoteget.ps1
+++ xfer/scripts/remoteget.ps1
@@ -1,2 +1,1 @@
-# Written by Harley Schaeffer version 1.2
 Start-Process PowerShell -ArgumentList "-windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted -File ""REPLACEME\scripts\get.ps1"" -gRemote TRUE"
