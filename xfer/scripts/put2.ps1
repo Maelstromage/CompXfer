4ef28f19746820a2cb2c116eabf22fe190ce6de4
Maelstromage <Reverse91@yahoo.com>
2019-06-05 09:23:00 -0400

    Update put2.ps1

--- xfer/scripts/put2.ps1
+++ xfer/scripts/put2.ps1
@@ -35,7 +35,10 @@ $creds = ""
 $scriptRoot = $PSScriptRoot
 ### error checking for confxfer.conf and adds values
 
+. $scriptRoot\verifyconf.ps1 
 
+
+<#
 
 foreach ($configLine in $compXfer) {
     $lineCountXfer++
@@ -211,7 +214,7 @@ foreach ($configLine in $compXfer) {
     
     
 }
-
+#>
 
 Write-Host -fore gray "Loading $cPath..."
 
